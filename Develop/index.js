26a1ba2b6747afb53baf087eb70e285d7786fe1e
Filip <f2alhama@uwaterloo.ca>
2021-10-12 13:01:49 -0400

    formatting complete

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = ["What is the title of your project?", "What was your motivation behind the creation of this project?", "Why did you build this project?", "What problem does this project solve?", "What did you learn in doing this project?", "What are the steps required to install your project?", "How would you use your project? Provide examples.", "Who were your collaborators if any and what were their GitHub URL's in the format 'name:url' ?", "What third party assets did you use if any?", "What permissions and restrictions do you give other developers with your project?", "What testing did you do on your project?", "What is your GitHub username?", "What is your email address?", "What is the best way for people to reach out to you with questions?"];
+const questions = ["What is the title of your project?", "What was your motivation behind the creation of this project?", "Why did you build this project?", "What problem does this project solve?", "What did you learn in doing this project?", "What are the steps required to install your project?", "How would you use your project? Provide examples.", "Who were your collaborators if any and what were their GitHub URL's in the format 'name: url,' ?", "What third party assets did you use if any?", "What permissions and restrictions do you give other developers with your project?", "What testing did you do on your project?", "What is your GitHub username?", "What is your email address?", "What is the best way for people to reach out to you with questions?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -121,7 +121,7 @@ function init() {
         writeToFile(response.title, 
 `
 # ${response.title}\n
-# ${generateMarkdown(response.licenseList)}\n
+${generateMarkdown(response.licenseList)}\n
 ## Description\n
 - ${response.description1}
 - ${response.description2}
@@ -153,8 +153,8 @@ This project is protected under the ${response.licenseList} license
 ${response.testing}
 \n
 ## Questions\n
-<a href="https://github.com/${response.questions1}">My GitHub</a>
-<a href="${response.questions2}">My Email</a>\n
+* My GitHub: (https://github.com/${response.questions1}")
+* My Email (${response.questions2})\n
 ${response.questions3}
 `
             )
